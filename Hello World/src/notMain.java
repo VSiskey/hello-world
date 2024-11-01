df4ad005f141e1e07e4668a487d78ec3404529fc
VSiskey <appenwagosh@gmail.com>
2024-11-01 21:15:19 +0300

    решено 5 задач

---
+++ Hello World/src/notMain.java
@@ -0,0 +1,16 @@
+public class notMain {
+    public static void main(String[] args) {
+        int i = 5;
+        String a = "332";
+        boolean s = true;
+        short q = 2;
+        float w = 2.3F;
+        char j  = ' ';
+
+        boolean h = (5 < 2);
+
+
+        System.out.println(h);
+
+    }
+}
